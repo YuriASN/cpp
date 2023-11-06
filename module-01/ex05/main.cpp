cb4d6c7292b2e9045a10fc129afaeefb1550a80d
Yuri Nunes <yuriasn@hotmail.com>
2023-11-06 22:11:35 +0000

    Fixed harl... sort of heheh

--- module-01/ex05/main.cpp
+++ module-01/ex05/main.cpp
@@ -5,11 +5,16 @@ int	main(void)
 	Harl	karen;
 	std::string	input;
 
-	while (input != "exit")
-	{
-		std::cout << "What Harl will do?" << std::endl;
-		std::getline(std::cin, input);
-		karen.complain(input);
-	}
+	std::cout << "Time for harl to debug..." << std::endl;
+	karen.complain("debug");
+
+	std::cout << "Time for harl to give info..." << std::endl;
+	karen.complain("info");
+
+	std::cout << "Time for harl to give an warning..." << std::endl;
+	karen.complain("warning");
+	
+	std::cout << "Time for harl to give an error..." << std::endl;
+	karen.complain("error");
 	return (0);
 }
